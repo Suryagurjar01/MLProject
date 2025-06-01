cf1dbdbbedc967de6d5ece74717f3e4b7e98b457
Suryagurjar01 <suryaveergurjar786@gmail.com>
2025-06-01 11:28:52 +0530

    EDA and PROBLEM STATEMENT

--- setup.py
+++ setup.py
@@ -8,14 +8,14 @@ def get_requirements(file_path:str)->list[str]:
     requirements=[]
     with open(file_path) as file_obj:
         requirements=file_obj.readlines()
-        [req.replace("\n","") for req in requirements]
+        requirements=[req.replace("\n","") for req in requirements]
         
         if HYPEN_E_DOT in requirements:
             requirements.remove(HYPEN_E_DOT)
 
     return requirements
 setup(
-    name='mlproject',
+    name='MLPROJECT',
     version='0.0.1',
     author='suryaveer',
     author_email='suryaveergurjar786@gmail.com',
